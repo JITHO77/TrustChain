32d026ce81ce16d91ba455d5c7b3f9361a0c276f
jitho v joy <jithovjoy@gmail.com>
2020-07-28 19:16:11 +0530

    DonateHomeComponent part 1

---
+++ src/components/donateHomeComponent.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+
+import {Card, CardImg, CardText, CardBody, CardLink, CardTitle, CardSubtitle, Col} from 'reactstrap';
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
+class DonateHome extends Component{
+    constructor(props){
+        super(props);
+        this.setState={
+
+        }
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6 mt-3 mb-3">
+                
+                        <Card>
+                        <CardBody>
+                            <CardTitle><b>MEDICAL</b></CardTitle>
+                            <CardSubtitle>Donate To Save A Life</CardSubtitle>
+                        </CardBody>
+                        <img width="100%" src={this.props.images.medical} alt="Card image cap" />
+                        <CardBody>
+                            <CardText>Click Below To Donate</CardText>
+                            
+                                    <Button component={Link} to="" color="primary" >Donate</Button>
+                                
+                        </CardBody>
+                        </Card>
+                    </div>
+
+                    <div className="col-12 col-md-6 mt-3 mb-3">
+                
+                        <Card>
+                        <CardBody>
+                            <CardTitle><b>FOR SHELTER</b></CardTitle>
+                            <CardSubtitle>Donate To Build House</CardSubtitle>
+                        </CardBody>
+                        <img width="85%" src={this.props.images.house} alt="Card image cap" />
+                        <CardBody>
+                            <CardText>Click Below To Donate</CardText>
+                            
+                                    <Button type="button" color="primary">Donate</Button>
+                                
+                        </CardBody>
+                        </Card>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-6 mt-3 mb-3">
+                
+                        <Card>
+                        <CardBody>
+                            <CardTitle><b>FOR ORPHANAGE</b></CardTitle>
+                            <CardSubtitle>Donate To Orphanages</CardSubtitle>
+                        </CardBody>
+                        <img width="54.5%" src={this.props.images.orphanage} alt="Card image cap" />
+                        <CardBody>
+                            <CardText>Click Below To Donate</CardText>
+                            
+                                    <Button type="button" color="primary">Donate</Button>
+                                
+                        </CardBody>
+                        </Card>
+                    </div>
+
+                    <div className="col-12 col-md-6 mt-3 mb-3">
+                
+                        <Card>
+                        <CardBody>
+                            <CardTitle><b>FOR FARM</b></CardTitle>
+                            <CardSubtitle>Donate To Farmers</CardSubtitle>
+                        </CardBody>
+                        <img width="98%" src={this.props.images.farm} alt="Card image cap" />
+                        <CardBody>
+                            <CardText>Click Below To Donate</CardText>
+                            
+                                    <Button type="button" color="primary">Donate</Button>
+                                
+                        </CardBody>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export  default DonateHome;
--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -4,6 +4,7 @@ import Footer from './footerComponent';
 import Home from './homeComponent';
 import Medical from "./medical/medicalComponent";
 import RequestHome from './requestHomeComponent';
+import DonateHome from './donateHomeComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {incStatus, savePersonalDetails} from '../redux/ActionCreater';
@@ -30,8 +31,9 @@ class Main extends Component{
              <Switch>      
              <Route path="/home" component = {()=><Home images={this.props.images}/>}/>
              <Route path="/request" component = {()=><RequestHome images={this.props.images}/>}/>
+             <Route path="/donate" component = {()=><DonateHome images={this.props.images}/>}/>
              <Route path="/requestMedical" component = {() => <Medical person={this.props.person} savePersonalDetails={this.props.savePersonalDetails} />} />
-            
+
              <Redirect to="/home" />
              </Switch>
              <Footer />
